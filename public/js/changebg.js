8dbe0613641097d3cc9921aabfc36cefeae347d5
wangbo <advance345@126.com>
2014-01-23 18:31:03 +0800

    增加了背景色

---
+++ public/js/changebg.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 14-1-23
+ * Time: 下午1:41
+ * To change this template use File | Settings | File Templates.
+ */
+$(function(){
+    var body = $("body");
+    body.css("background-image", "url('/img/cartographer.png')");
+    body.css("background-repeat", "repeat");
+    body.css("background-attachment", "fixed");
+    var modal = $("changebgModal");
+});
