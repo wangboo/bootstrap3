063a0e241f504d13bd3b957ad27c1f8fb2940cdb
wangboo <advance345@126.com>
2014-01-24 00:04:50 +0800

    增加了切换背景的功能（实现方式很丑陋）

--- public/js/changebg.js
+++ public/js/changebg.js
@@ -6,9 +6,10 @@
  * To change this template use File | Settings | File Templates.
  */
 $(function(){
-    var body = $("body");
-    body.css("background-image", "url('/img/cartographer.png')");
-    body.css("background-repeat", "repeat");
-    body.css("background-attachment", "fixed");
-    var modal = $("changebgModal");
+
+
 });
+function changebg(img) {
+    $("body").css("background-image", "url('"+img+"')");
+    $("#changebgModal").modal('toggle');
+}
