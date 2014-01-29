3f6cdcef7cea4c5b5d1723e6bff99b7120ad01a2
wangboo <advance345@126.com>
2014-01-29 18:16:42 +0800

    增加了发帖页面，富文本控件采用 umeditor see https://github.com/campaign/umeditor

--- controllers/main.js
+++ controllers/main.js
@@ -7,3 +7,9 @@ exports.war3 = function(req, res) {
     req.crumb.add("war3", "/node/war3");
     res.render('node');
 }
+exports.new = function(req, res) {
+    res.render('new');
+}
+exports.create = function(req, res) {
+    console.log(req.body);
+}
--- routes.js
+++ routes.js
@@ -5,5 +5,7 @@ module.exports = function(app) {
     return function(app) {
         app.get("/", main.index);
         app.get("/node/war3", main.war3);
+        app.get("/item/new", main.new);
+        app.post("/item/create", main.create);
     }
 }
